d3c2a77c2b533409f07af906ec024fbd2d1f0a86
213311012 <numangunduz00@gmail.com>
2024-02-22 12:20:37 +0300

    SignalRApi 'nin Projeye Dahil Edilmesi

---
+++ SignalRApi/DAL/Visitor.cs
@@ -0,0 +1,19 @@
+﻿namespace SignalRApi.DAL
+{
+    public enum Ecity
+    {
+        Edirne =1,
+        İstanbul=2,
+        Ankara=3,
+        Antalya=4,
+        Bursas=5
+        
+    }
+    public class Visitor
+    {
+        public int VisitorID { get; set; }
+        public Ecity City { get; set; }
+        public int CityVisitCount { get; set; }
+        public DateTime VisitDate { get; set; }
+    }
+}
